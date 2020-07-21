05ed5e64919b00b5e94417995424a2fd26460092
braillonw <braillonw@gmail.com>
2020-07-21 10:32:55 +0200

    nouveau paragraphe dans le footer

---
+++ wp-content/themes/montheme/footer.php
@@ -0,0 +1,14 @@
+</div>
+<footer>
+  <?php wp_nav_menu([
+    'theme_location' => 'footer',
+    'container' => false,
+    'menu_class' => 'navbar-nav mr-auto'
+  ]);
+  ?>
+  <p>hello</p>
+</footer>
+<?php wp_footer() ?>
+</body>
+
+</html>
